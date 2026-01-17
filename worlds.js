d81f5ec0a6c087eeaa1465d949933823b01108a4
fitzgeraldmchauke-hub <fitzgeraldmchauke@gmail.com>
2026-01-17 14:27:58 +0000

    Update worlds.js

--- worlds.js
+++ worlds.js
@@ -1,6 +1,8 @@
-function WorldSystem(scene){
-  window.loadWorld=(name)=>{
-    console.log("Loading world:",name);
-    // placeholder: later replace blocks / NPCs
+function WorldSystem(){
+  window.World = {
+    load: (name) => {
+      console.log("Loading world:", name);
+      // placeholder: later replace blocks / NPCs
+    }
   }
 }
