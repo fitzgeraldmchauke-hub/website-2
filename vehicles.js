c3d7a21926683834fb1aee101438e02f7edd5567
fitzgeraldmchauke-hub <fitzgeraldmchauke@gmail.com>
2026-01-17 14:26:35 +0000

    Update vehicle controls to use lowercase arrow keys

--- vehicles.js
+++ vehicles.js
@@ -1,13 +1,15 @@
 function VehicleSystem(scene){
   const vehicle = BABYLON.MeshBuilder.CreateBox("car",{size:1.5},scene);
   vehicle.position.set(0,1,5);
+  vehicle.checkCollisions = true;
 
   scene.onBeforeRenderObservable.add(()=>{
+    if(typeof keyIsDown !== "function") return; // safety
     let dir=0;
-    if(keyIsDown("ArrowUp")) dir+=0.1;
-    if(keyIsDown("ArrowDown")) dir-=0.1;
+    if(keyIsDown("arrowup")) dir+=0.1;
+    if(keyIsDown("arrowdown")) dir-=0.1;
     vehicle.position.z += dir;
-    if(keyIsDown("ArrowLeft")) vehicle.position.x -=0.1;
-    if(keyIsDown("ArrowRight")) vehicle.position.x +=0.1;
+    if(keyIsDown("arrowleft")) vehicle.position.x -=0.1;
+    if(keyIsDown("arrowright")) vehicle.position.x +=0.1;
   });
 }
